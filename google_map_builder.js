c43d00a61d7a401eb0c1b14368ed6dd2928edbcf
Craig Carlson <craigsidcarlson@gmail.com>
2020-06-09 16:21:57 -0700

    Added feature to calculate the earth radius at the start of each segment

--- google_map_builder.js
+++ google_map_builder.js
@@ -1,5 +1,5 @@
 
-const { getPreciseDistance, getRhumbLineBearing, computeDestinationPoint } = require('geolib');
+const { getPreciseDistance, getGreatCircleBearing, computeDestinationPoint } = require('geolib');
 const axios = require('axios');
 const fs = require('fs');
 require('dotenv').config();
@@ -19,15 +19,18 @@ const file_prefix = 'roz_data';
 const start_lat = 41.30;
 const start_long = -131.300;
 
-const end_lat = 39.10;
+const end_lat = 40.90;
 const end_long = -131.30;
 
 const increments = 50; // meters
+
+const POLAR_RADIUS = 6356752 // meters
+const EQUATORIAL_RADIUS = 6378137 // meters
 ////////////////////////////////////////
 
 //  Vincenty inverse formula for ellipsoids.
 const distance = getPreciseDistance({latitude: start_lat, longitude: start_long}, {latitude: end_lat, longitude: end_long});
-const compass_direction = getRhumbLineBearing({latitude: start_lat, longitude: start_long}, {latitude: end_lat, longitude: end_long});
+const compass_direction = getGreatCircleBearing({latitude: start_lat, longitude: start_long}, {latitude: end_lat, longitude: end_long});
 
 console.log(`Distance ${distance}`);
 console.log(`Bearing ${compass_direction}`);
@@ -40,16 +43,18 @@ console.log(`Stitching together ${segments} segments`);
 
 const promises = [];
 let start = { latitude: start_lat, longitude: start_long };
+let earth_radius_with_lat = calculateEarthRadiusAtLat(start.latitude);
 let end;
 for(let i = 0; i < segments; i++) {
 
 	if(i === segments - 1) end = {latitude: end_lat, longitude: end_long};
 	else {
 		// computeDestinationPoint(point, distance, bearing, radius = earthRadius)
-		end = computeDestinationPoint({latitude: start.latitude, longitude: start.longitude}, segment_length, compass_direction);
+		end = computeDestinationPoint({latitude: start.latitude, longitude: start.longitude}, segment_length, compass_direction, earth_radius_with_lat);
 	}
 	promises.push(callGoogleApi(start, end, google_api_limit));
 	start = end;
+	earth_radius_with_lat = calculateEarthRadiusAtLat(start.latitude);
 }
 
 Promise.all(promises)
@@ -97,4 +102,32 @@ function callGoogleApi(start, end, google_api_limit) {
 				return reject(error);
 			})
 	});
+}
+
+function calculateEarthRadiusAtLat(lat) {
+	const equatorial_numerator = Math.pow((Math.pow(EQUATORIAL_RADIUS, 2) * cosine(lat)), 2);
+	const polar_numberator = Math.pow((Math.pow(POLAR_RADIUS, 2) * sine(lat)), 2);
+
+	const equatorial_denominator = Math.pow((EQUATORIAL_RADIUS * cosine(lat)), 2);
+	const polar_denominator = Math.pow((POLAR_RADIUS * sine(lat)), 2);
+
+	const geocentric_radius = Math.sqrt(((equatorial_numerator + polar_numberator)/(equatorial_denominator + polar_denominator)));
+	console.log(`Geocentric Radius at latitude: ${lat} = ${geocentric_radius}`);
+	return geocentric_radius;
+}
+
+function toDegrees(angle) {
+  return angle * (180 / Math.PI);
+}
+
+function toRadians (angle) {
+  return angle * (Math.PI / 180);
+}
+
+function cosine(angle) {
+	return toDegrees(Math.cos(toRadians(angle)));
+}
+
+function sine(angle) {
+	return toDegrees(Math.sin(toRadians(angle)));
 }
